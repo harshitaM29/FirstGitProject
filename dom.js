c35419fce45a2e9c42d55d6bb0c20abd04217528
harshitaM29 <harshita.mundhe29@gmail.com>
2023-04-18 20:38:34 +0530

    Added files in git gor first Dom project

---
+++ dom.js
@@ -0,0 +1,28 @@
+// console.dir(document)
+
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = 123;
+
+// console.log(document.doctype)
+// console.log(document.head)
+// console.log(document.body)
+// console.log(document.all)
+// document.all[10].textContent = "HELLO";
+// console.log(document.forms[0])
+// console.log(document.links)
+
+
+//console.log(document.getElementById('header-title'))
+
+var headerTitle = document.getElementById('header-title')
+var header = document.getElementById('main-header')
+
+// headerTitle.textContent = 'HELLO'
+
+header.style.borderBottom = 'solid 3px #000';
+
+var items = document.getElementsByClassName('title');
+items[0].style.fontWeight = 'bold'
+items[0].style.color = 'green'
+
