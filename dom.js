856c1bd75e5931827294b09a49625c7d5c260f6e
harshitaM29 <harshita.mundhe29@gmail.com>
2023-04-20 23:09:21 +0530

    Changes for displaying input on same screen

--- dom.js
+++ dom.js
@@ -1,5 +1,6 @@
-function test()
+function test(event)
 {
+    event.preventDefault();
     var name = document.getElementById('name').value;
     var em = document.getElementById('email').value;
     var ph = document.getElementById('phone').value;
@@ -7,10 +8,30 @@ function test()
     var tm = document.getElementById('time').value;
 
     let myObj = {name,em,ph,dt,tm};
+    localStorage.clear();
 
     let myObj_serialized = JSON.stringify(myObj)
+    console.log(myObj_serialized)
 
-    localStorage.setItem('myobj',myObj_serialized)
+    localStorage.setItem('myObj',myObj_serialized)
+   
+    let myObj_dserialized = JSON.parse(localStorage.getItem('myObj'));
+    var nm = myObj_dserialized.name
+    var a = myObj_dserialized.em;
+    var b = myObj_dserialized.ph
+    var fm = document.getElementById('form');
+    var para = document.createElement('pre');
+    para.style.fontSize = '20 px'
+    para.style.fontWeight = 'bold'
+    para.textContent = "Name = " + nm + " Email = " + a + " Phone = " + b;
+    fm.append(para)
+
+   
+
+   
+
+    
+
     
 
 
