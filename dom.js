1fe782fdbeb9349893e3c7cd74197574070376dc
harshitaM29 <harshita.mundhe29@gmail.com>
2023-04-20 19:01:58 +0530

    code for adding description and searching by same

--- dom.js
+++ dom.js
@@ -1,5 +1,7 @@
 var form = document.getElementById('addForm');
 var  itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+
 
 // form submit event
 form.addEventListener('submit', addItem);
@@ -7,18 +9,26 @@ form.addEventListener('submit', addItem);
 
 itemList.addEventListener('click', removeItem);
 
+//filter event
+filter.addEventListener('keyup',filterItems);
+
+
 
 
 function addItem(e) {
     e.preventDefault();
     var newItem = document.getElementById('item').value;
 
+    var newDescription = document.getElementById('description').value;
+
     //create new li element
     var li = document.createElement('li');
 
     li.className = 'list-group-item';
     li.appendChild(document.createTextNode(newItem));
-    
+    li.appendChild(document.createTextNode(" "));
+    li.appendChild(document.createTextNode(newDescription));
+
     var edit = document.createElement('button');
     edit.className = 'btn btn-primary btn-sm float-right edit';
     edit.appendChild(document.createTextNode('E'));
@@ -50,5 +60,24 @@ function removeItem(e)
     }
 }
 
+function filterItems(e)
+{
+    var text =  e.target.value.toLowerCase();
+    var items = itemList.getElementsByTagName('li');
+    Array.from(items).forEach(function(item ) {
+        var itemName = item.firstChild.textContent;
+        var itemSecond = item.childNodes[0].textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1 || itemSecond.toLowerCase().indexOf(text) != -1)
+        {
+            item.style.display = 'block';
+        }
+       
+        else{
+            item.style.display = 'none';
+        }
+
+    });
+
+}
 
 
