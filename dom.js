2897acd27c90d9d573db8df014b47747b13e64db
harshitaM29 <harshita.mundhe29@gmail.com>
2023-05-12 19:00:52 +0530

    EDIT functionality

--- dom.js
+++ dom.js
@@ -15,7 +15,7 @@ function test(event)
 
     // localStorage.setItem('myObj',myObj_serialized)
 
-    axios.post("https://crudcrud.com/api/3ea7499aeb1340a39e21f98d1594cbdb/appointmendDetails", myObj)
+    axios.post("https://crudcrud.com/api/37a29ba8081b4c1b8cf538a740a0f730/appointmendDetails", myObj)
     .then((reponse => {
         showOnScreen(reponse.data)
         console.log(reponse);
@@ -31,7 +31,7 @@ function test(event)
     // showOnScreen(myObj);
 }
 window.addEventListener("DOMContentLoaded", () => {
-    axios.get("https://crudcrud.com/api/3ea7499aeb1340a39e21f98d1594cbdb/appointmendDetails")
+    axios.get("https://crudcrud.com/api/37a29ba8081b4c1b8cf538a740a0f730/appointmendDetails")
     .then((response) => {
         for(var i=0;i<response.data.length; i++){
             showOnScreen(response.data[i]);
@@ -45,11 +45,7 @@ window.addEventListener("DOMContentLoaded", () => {
 function showOnScreen(myObj)
 {
     // let myObj_dserialized = JSON.parse(localStorage.getItem('myObj'));
-    var nm;
-    var a;
-    var b ;
-    // var c = myObj.dt;
-    // var d = myObj.tm;
+  
     const ul = document.getElementById('list');
     const li = document.createElement('li');
     // para.style.fontSize = '20 px'
@@ -66,7 +62,7 @@ function showOnScreen(myObj)
         if(confirm('are you sure'))
                 {
                     
-                    axios.delete(`https://crudcrud.com/api/3ea7499aeb1340a39e21f98d1594cbdb/appointmendDetails/${myObj._id}`)
+                    axios.delete(`https://crudcrud.com/api/37a29ba8081b4c1b8cf538a740a0f730/appointmendDetails/${myObj._id}`)
                     .then((res) => console.log(res))
                     .catch((err) => console.log(err)) 
                     var li = e.target.parentElement;
@@ -81,28 +77,46 @@ function showOnScreen(myObj)
     btnEdit.textContent = 'EDIT'
     btnEdit.style.borderRadius = '5 px'
     btnEdit.style.border = 'black'
-    // btnEdit.addEventListener("click", function(e){
+    btnEdit.addEventListener("click", function(e){
         
                 
-    //                 localStorage.clear();
-    //                 var li = e.target.parentElement;
-    //                 var item = document.getElementById('list');
-    //                 item.removeChild(li);
-    //                 var textField1 = document.getElementById('name')
-    //                 var emailField = document.getElementById('email');
-    //                 var phoneField = document.getElementById('phone');
-    //                 var dateField = document.getElementById('date');
-    //                 var timeField = document.getElementById('time');
-    //                 textField1.value = nm;
-    //                 emailField.value = a;
-    //                 phoneField.value = b;
-    //                 dateField.value = c;
-    //                 timeField.value = d;
+                   
+                    var li = e.target.parentElement;
+                    var item = document.getElementById('list');
+                    // item.removeChild(li);
+                    var textField1 = document.getElementById('name')
+                    var emailField = document.getElementById('email');
+                    var phoneField = document.getElementById('phone');
+                    var dateField = document.getElementById('date');
+                    var timeField = document.getElementById('time');
+                    textField1.value = myObj.name;
+                    emailField.value =myObj.em;
+                    phoneField.value = myObj.ph;
+                    dateField.value = myObj.dt;
+                    timeField.value = myObj.tm;
+                    var newName = textField1.value;
+                    var newEmail = emailField.value;
+                    var newPhone = phoneField.value
+                    let myObj1 = [newName,newEmail,newPhone]
+                    document.getElementById('submit').value = 'Edit'      
+                    var fm = document.getElementById('form');
+                    fm.addEventListener("submit", myFunction)
+                    function myFunction()
+                    {
+                        axios.put(`https://crudcrud.com/api/37a29ba8081b4c1b8cf538a740a0f730/appointmendDetails/${myObj._id}`, {
+                            name: newName,
+                            em : newEmail,
+                            ph : newPhone
+                        })
+                        .then((response) => {
+                            console.log(response.data)
+                        })
+                    }
 
 
                     
                 
-    // });
+    });
     li.append(" ",btnEdit)
 
 }
